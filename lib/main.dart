afe8e72519f07e07e743cb08eb2523c733f189e2
Alexander Ryzhov <aryzhov07@gmail.com>
2019-06-01 12:34:51 -0700

    Added Navigator

--- lib/main.dart
+++ lib/main.dart
@@ -11,12 +11,25 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
 
-TabController tabController;
+  final navigatorKey = GlobalKey<NavigatorState>(debugLabel: "Navigator");
+  final navigatorPage = PageTracker();
+  TabController tabController;
 
   @override
   void initState() {
     super.initState();
-     tabController = TabController(length: pages.length, vsync: this);
+    tabController = TabController(length: pages.length, vsync: this);
+    tabController.addListener(() {
+      if(!tabController.indexIsChanging) {
+        final page = tabController.index;
+        final nav = navigatorKey.currentState;
+        if(navigatorPage.page != page) {
+          final pageName = page == 0 ? '': '/${pages[page]}';
+          print("Push replacement: $pageName");
+          nav.pushReplacementNamed(pageName);
+        }
+      }
+    });
   }
 
   @override
@@ -32,9 +45,37 @@ TabController tabController;
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(tabController),
+      navigatorKey: navigatorKey,
+      initialRoute: '',
+      onGenerateRoute: _generateRoute,
+      navigatorObservers: [
+        navigatorPage,
+      ],
     );
   }
+
+  Route _generateRoute(RouteSettings settings) {
+    final int page = getPageFromRoute(settings);
+    return MaterialPageRoute(
+      settings: RouteSettings(name: settings.name, isInitialRoute: true),
+      builder: (context) {
+        if(page != null) {
+          WidgetsBinding.instance.addPostFrameCallback((_) => goToPage(page));
+          return MyHomePage(tabController,);
+        } else {
+          return PageNotFound(settings.name);
+        }
+      },
+    );
+  }
+
+  goToPage(int page) {
+    if(tabController.index != page)  {
+      print("Set tab index: $page");
+      tabController.index = page;
+    }
+  }
+
 }
 
 class MyHomePage extends StatefulWidget {
@@ -53,7 +94,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Hello"),
+        title: Text("Flutter Web TabBar + Navigator Solution Demo"),
         bottom: TabBar(
           controller: widget.tabController, 
           tabs: [
@@ -83,6 +124,74 @@ class SomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Center(
       child: Text(text,),
+    );
+  }
+
+}
+
+int getPageFromRoute(RouteSettings rs) {
+  if(rs.name == null) return null;
+  final pageName = rs.name.startsWith('/') ? rs.name.substring(1): rs.name;
+  final page = pageName == '' ? 0 : pages.indexOf(pageName);
+  return page >= 0 ? page: null;
+}
+
+class PageTracker extends NavigatorObserver {
+
+  int page = 0;
+
+  @override
+  void didPop(Route route, Route previousRoute) {
+    page = getPageFromRoute(previousRoute.settings) ?? page;
+  }
+
+  @override
+  void didPush(Route route, Route previousRoute) {
+    page = getPageFromRoute(route.settings) ?? page;
+  }
+
+  @override
+  void didRemove(Route route, Route previousRoute) {
+    page = getPageFromRoute(previousRoute.settings) ?? page;
+  }
+
+  @override
+  void didReplace({Route newRoute, Route oldRoute}) {
+    page = getPageFromRoute(newRoute.settings) ?? page;
+  }
+
+  @override
+  void didStartUserGesture(Route route, Route previousRoute) {
+    // nothing
+  }
+
+  @override
+  void didStopUserGesture() {
+    // nothing
+  }
+
+}
+
+class PageNotFound extends StatelessWidget {
+
+  final String pageName;
+
+  PageNotFound(this.pageName);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text("Page not found: $pageName"),
+            RaisedButton(child: Text("Home Page"), onPressed: () {
+              Navigator.pushReplacementNamed(context, '');
+            },),
+          ],
+        ),
+      ),
     );
   }
 
